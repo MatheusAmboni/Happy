353fd83dfe86df11bfcef66c1c84821db4d2f9ce
MatheusAmboni <mateussamboni@gmail.com>
2020-10-16 10:13:44 -0300

    Map change

--- web/src/screens/OrphanageMap.tsx
+++ web/src/screens/OrphanageMap.tsx
@@ -33,7 +33,7 @@ function OrphanageMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer 
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
             </Map>
 
